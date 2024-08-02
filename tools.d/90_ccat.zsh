420d20e61d94571189c7a8958a35b59277acb9e2
Antonio Abella <aabel011@gmail.com>
2024-08-02 19:56:51 -0400

    ccat & cless - move funcs to tools.d and support pipe/stdin

---
+++ tools.d/90_ccat.zsh
@@ -0,0 +1,36 @@
+function ccat () {
+    local cmd
+    if command -v pygmentize &>/dev/null; then
+        cmd="pygmentize -g -O style=nord-darker"
+    elif command -v chroma &>/dev/null; then
+        cmd="chroma --style=dracula --formatter=terminal256"
+    else
+        echo "error: pygmentize or chroma not installed" >&2
+        return 1
+    fi
+    if [[ -f "$@[-1]" ]]; then
+        local file="$@[-1]"
+        eval $cmd "$file"
+    else
+        cat | eval $cmd
+    fi
+}
+function cless () {
+    # TODO: handle flags before or after file
+    case "$@[-1]" in
+        -*|"")
+            ccat | less -R $@
+        ;;
+        *)
+            if [[ -f "$@[-1]" ]]; then
+                local file="$@[-1]"
+                ccat "$file" | less -R ${@[@]:1:-1}
+            else
+                echo "error: file not found: $file" >&2
+                return 1
+            fi
+        ;;
+    esac
+}
+compdef _less cless
+
