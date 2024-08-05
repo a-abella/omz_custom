09597e552a694b543a2086e1ba9c24dc59672498
Antonio Abella <aabel011@gmail.com>
2024-08-04 21:10:02 -0400

    confs - add custom confs dir, create support for custom tmux conf

--- aliases.zsh
+++ aliases.zsh
@@ -4,6 +4,8 @@
 alias lla="ll -a"
 alias l='lla'
 
+alias tmux='tmux -f "$TMUX_CONF"'
+
 # k8s stuff
 alias kdumpall="kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found --all-namespaces"
 alias kcuncc="kubectl config unset current-context"
--- exports.zsh
+++ exports.zsh
@@ -1,7 +1,11 @@
 # random exports
 
+# tmux conf location
+export TMUX_CONF="$HOME/.oh-my-zsh/custom/confs/tmux-custom.conf"
+
 # set LANG locale for CLI progs that care
-LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 
 # unset aws-cli pager, default to no paging
 export AWS_PAGER=
