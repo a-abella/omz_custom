f6b4f862f51d44d3405cef07d49730d2b1b8ead5
Antonio Abella <aabel011@gmail.com>
2024-08-07 20:25:16 -0400

    add random aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,11 +1,22 @@
 # simple aliases
 #   functions should go in tools.zsh or tools.d/*.zsh
 
+# condensers
+alias l='ll'
+alias la='ll -a'
+alias lr='ll -R'
+alias lar='ll -aR'
 alias lla="ll -a"
-alias l='lla'
+alias llr='ll -R'
+alias llar='ll -aR'
 
+# command overrides
 alias tmux='tmux -f "$TMUX_CONF"'
+
+# oneline funcs
+alias mtime='_mtime(){ local TIMEFMT="%J  %mU user %mS system %P cpu %mE total"; time $@; unset _mtime; }; _mtime'
 
 # k8s stuff
 alias kdumpall="kubectl api-resources --verbs=list --namespaced -o name | xargs -n 1 kubectl get --show-kind --ignore-not-found --all-namespaces"
 alias kcuncc="kubectl config unset current-context"
+alias kcontext="kubectl config use-context"
