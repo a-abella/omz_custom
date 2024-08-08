5230f38fa15e29b1cbffd048a138e6207bc5f2c3
Antonio Abella <aabel011@gmail.com>
2024-08-08 18:27:21 -0400

    completions - add a place to source random completions

---
+++ completions.zsh
@@ -0,0 +1,5 @@
+# for any cmds (usually installed by brew) that do
+# not auto-source completions from $FPATH, completion
+# sourcing can be done here.
+
+source $(brew --prefix)/etc/bash_completion.d/az
