afbdb35649df04f6d3908e113e6d55d9bc726cd9
Antonio Abella <aabel011@gmail.com>
2024-08-15 13:11:52 -0400

    tools - smarter sourcing

--- tools.zsh
+++ tools.zsh
@@ -1,10 +1,15 @@
+# source all tools.d/*.zsh
 tools_d="$HOME/.oh-my-zsh/custom/tools.d"
 if [[ -d "$tools_d" ]]; then
+  setopt +o nomatch
+  local _paths=( "$tools_d"/*.zsh "$tools_d"/*.sh )
+  setopt nomatch
   local _file
-  for _file in "$tools_d"/* ; do
-    source "$_file"
+  for _file in $_paths ; do
+    [[ -s "$_file" ]] && source "$_file"
   done
   unset _file
+  unset _paths
 fi
 
 function sshcheck() {
